8eb7aea2cb576c21fdaf9dd34f85a27f120c14a9
Bruna Ceppa <brunaceppa116@gmail.com>
2022-12-06 11:31:12 -0300

    comienzo con gestionUsusarios

--- inmobiliaria-front/src/Pages/GestionUsuarios/GestionUsuarios.jsx
+++ inmobiliaria-front/src/Pages/GestionUsuarios/GestionUsuarios.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import HeaderAdmin from '../../Components/Header/HeaderAdmin/HeaderAdmin';
+import React from "react";
+import HeaderAdmin from "../../Components/Header/HeaderAdmin/HeaderAdmin";
 
 import {
   Card,
@@ -14,18 +14,25 @@ import {
   Image,
 } from "@chakra-ui/react";
 
-
 function GestionUsuarios() {
   return (
     <>
-    <HeaderAdmin/>
-      
-      <Flex>
+      <HeaderAdmin />
 
+      <Flex flexDirection="column" justifyContent="center" mx="27rem" my="20">
+        <Box>
+          <Text fontSize="2rem">Tablero Administrativo</Text>
+          <Text fontSize="3rem" as="b">
+            Usuarios
+          </Text>
+        </Box>
+
+        <Button bg="var(--red)" variant="solid" borderRadius='3rem' px='5' py='10' w='10%' fontSize='1.6rem'>
+          Crear usuario
+        </Button>
       </Flex>
-      
-      </>
-  )
+    </>
+  );
 }
 
 export default GestionUsuarios;
