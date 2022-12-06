b77d2a05afb24acc663b7e9e5dd198293c598647
Bruna Ceppa <brunaceppa116@gmail.com>
2022-12-06 13:55:52 -0300

    arreglo de conflictos en back

--- inmobiliaria-back/routes/inmueblesRoutes.js
+++ inmobiliaria-back/routes/inmueblesRoutes.js
@@ -9,18 +9,16 @@ const {
     photo,
     buscarById,
     
-  } = require("../controllers/inmuebleControllers");
-  const { verifyToken } = require("../validators/auth");
-  const {verifyPerfil,verifyPerfil2} = require("../validators/perfil");
+  } = require("../controllers/inmuebleController");
+ 
  
   router.post("/inmueble/listainmueble/filtrar", filtrarInmueble);
-
  
    router.get("/inmuebles/listaInmueble", listaInmueble);
 
    router.get("/inmuebles/photo/:id", photo);
 
-   router.get("/inmuebles/buscar/:id", buscarById)
+   router.get("/inmuebles/buscar/:id", buscarById);
   
   router.post("/inmuebles/nuevo",  inmuebleNuevo);
   
--- inmobiliaria-back/routes/userRoutes.js
+++ inmobiliaria-back/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { listUser, login, register, infoUser, deleteUser } = require("../controllers/userController");
+const { listUser, login, register, infoUser, deleteUser } = require("../controllers/userControllers");
 const { runValidation } = require("../validators/index");
 
 const {
